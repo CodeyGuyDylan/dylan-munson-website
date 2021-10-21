e77a916bf8a965acfb85c1cbca9745d18c9135bb
CodeyGuyDylan <design@novysolutions.com>
2021-10-20 22:59:48 -0700

    added list of ways to contact

--- src/components/files/Contact.tsx
+++ src/components/files/Contact.tsx
@@ -1,5 +1,8 @@
+// Libraries
 import { Dispatch, FC, SetStateAction } from 'react'
+import styled from 'styled-components'
 
+// Forms
 import ContactForm from '../../form/forms/Contact'
 
 // Types
@@ -13,8 +16,43 @@ const Contact: FC<IContact> = ({ setAlert }) => {
    return (
       <>
          <ContactForm setAlert={setAlert} />
+
+         <Other>
+            <h2>Other ways to contact me</h2>
+
+            <ul>
+               <li>
+                  Twitter:{' '}
+                  <a href='https://twitter.com/dylanmunson8'>@dylanmunson8</a>
+               </li>
+
+               <li>
+                  LinkedIn:{' '}
+                  <a href='https://www.linkedin.com/in/dylan-munson-56597015b/'>
+                     Dylan Munson
+                  </a>
+               </li>
+
+               <li>
+                  Email:{' '}
+                  <a href='mailto:dylan@novysolutions.com'>
+                     dylan@novysolutions.com
+                  </a>
+               </li>
+
+               <li>
+                  Github:{' '}
+                  <a href='https://github.com/CodeyGuyDylan'>CodeyGuyDylan</a>
+               </li>
+            </ul>
+         </Other>
       </>
    )
 }
 
 export default Contact
+
+const Other = styled.section`
+   max-width: 500px;
+   margin: 5rem auto 0 auto;
+`
